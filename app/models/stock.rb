898ae82491d47d5ce14b9585eed4e3823085b459
KJTempas <kjtempas@gmail.com>
2021-06-26 16:24:36 -0500

    add front end for stock lookup search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,13 @@ class Stock < ApplicationRecord
                         publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key_publ],
                         secret_token: Rails.application.credentials.iex_client[:secret_access_key],
                         endpoint: 'https://sandbox.iexapis.com/v1')
-        client.price(ticker_symbol) 
+        #client.price(ticker_symbol) for initial testing
+        
+        begin #like Try and except in python; try to get a response; if not, throw an exception
+        #to make a new ticker object
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end
     end
 end
