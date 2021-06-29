29ba945c606135b3db271dd37ca5badb88fd257d
KJTempas <kjtempas@gmail.com>
2021-06-29 11:46:34 -0500

    update navbar;  add stocks in browser

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,2 +1,13 @@
 class UserStocksController < ApplicationController
+    def create
+        stock = Stock.check_db(params[:ticker]) #first look in db to see if stock already there
+        if stock.blank? # if not, lookup and use that to create new @user_stock object
+            stock = Stock.new_lookup(params[:ticker])
+            stock.save 
+        end
+        #now will have stock object either retrieved from db or created and saved
+        @user_stock = UserStock.create(user: current_user, stock: stock)
+        flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+        redirect_to my_portfolio_path
+    end
 end
